d5635edd8f7920b7917c11870053da329c45eabc
Gilles Ferrand <gilles.ferrand31@gmail.com>
2023-01-14 11:07:42 +0100

    Add useGlobalPipes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { MessagesModule } from './messages/messages.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(MessagesModule);
+  app.useGlobalPipes(new ValidationPipe())g;
   await app.listen(3000);
 }
 bootstrap();
